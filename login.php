c9136796ae54306c938d6b1812e712e78f4517eb
gigibardan <gheorghe.bardan@gmail.com>
2025-05-29 15:24:26 +0300

    fix login2

--- login.php
+++ login.php
@@ -4,8 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// IMPORTANT: Includem config.php PRIMUL pentru că auth.php depinde de el
-require_once 'config.php';
+// IMPORTANT: config.php este în includes/
+require_once 'includes/config.php';
 require_once 'includes/auth.php';
 
 // Verificăm dacă utilizatorul este deja autentificat
